bf45bdbf654f67382164a46db5ba657d27ed701a
MIchael <michael.olson756@gmail.com>
2016-08-18 10:03:11 -0600

    warmup complete

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,10 @@
-trig.controller("TrigController", function(){
+trig.controller("TrigController", function($scope, CalculatorFactory){
+  $scope.calculate = function(){
+    var sideB = $scope.sideB;
+    var sideA = $scope.sideA;
 
-  
+    $scope.hypotenuse = CalculatorFactory.add(sideA, sideB)
+
+}
 
 });
